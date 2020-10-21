703aabf62365aa96695e139a4267c9c34cf2addd
Andrew-Nally <Andrew_Nally@outlook.com>
2020-10-21 12:13:03 -0600

    corrected daily report

--- TechAcademyDailyReport/TechAcademyDailyReport/Program.cs
+++ TechAcademyDailyReport/TechAcademyDailyReport/Program.cs
@@ -15,50 +15,34 @@ namespace TechAcademyDR
             Console.Read();
 
             Console.WriteLine("What is your name?");
-            Console.ReadLine();
-            string myName = "Drew";
-            Console.ReadLine();
-            Console.WriteLine(myName);
-            Console.ReadLine();
+            string myName = Console.ReadLine();
+            
 
             Console.WriteLine("What course are you on?");
-            Console.ReadLine();
-            string courseName = "C#";
-            Console.WriteLine(courseName);
-            Console.ReadLine();
+            string courseName = Console.ReadLine();
+        
 
             Console.WriteLine("What page number?");
+            int pageNumber = pageNumber.ToString();
             Console.ReadLine();
-            int pageNumber = 133;
-            pageNumber.ToString();
-            Console.WriteLine(pageNumber);
-            Console.ReadLine();
-
+            
             Console.WriteLine("Do you need help with anything? Please answer true or false.");
-            Console.ReadLine();
-            bool courseHelp = false;
+            bool courseHelp = Console.ReadLine();
             courseHelp.ToString();
-            Console.WriteLine(courseHelp);
             Console.ReadLine();
 
             Console.WriteLine("Were there any positive experiences you would like to share? Please give specifics.");
-            Console.ReadLine();
-            string positiveExperience = "I totally appreciate the thorough explanations of all the C# terminology, concepts, etc. It totally helped clarify all the things I didnt understand the first time around making it so much more fun to learn";
-            Console.WriteLine(positiveExperience);
+            string positiveExperience = Console.ReadLine();
             Console.ReadLine();
 
             Console.WriteLine("Is there any other feedback you would like to share?");
-            Console.ReadLine();
-            bool feedback = false;
+            bool feedback = Console.ReadLine();
             feedback.ToString();
-            Console.WriteLine(feedback);
             Console.ReadLine();
 
             Console.WriteLine("How many hours did you study today?");
-            Console.ReadLine();
-            int hoursStudied = 10;
+            int hoursStudied = Console.ReadLine();
             hoursStudied.ToString();
-            Console.WriteLine(hoursStudied);
             Console.ReadLine();
 
             Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
