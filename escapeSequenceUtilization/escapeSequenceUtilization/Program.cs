e44c17da0893c65a7285befccfc1293355ac6d5b
Andrew-Nally <Andrew_Nally@outlook.com>
2020-10-21 04:16:45 -0600

    C# Assignments

---
+++ escapeSequenceUtilization/escapeSequenceUtilization/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main()
+        {
+        Console.WriteLine("In the words of Hamlet by Willam Shakespeare \"To be, or not to be, that is the question\" I end this assignment.");
+        Console.ReadLine();
+        }
+    }
+
