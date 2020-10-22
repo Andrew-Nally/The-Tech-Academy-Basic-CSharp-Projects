d8685e94746fe170338958143f00249ed6e359ec
Andrew-Nally <Andrew_Nally@outlook.com>
2020-10-22 00:29:51 -0600

    CarInsurance BooleanLogic

---
+++ Car_Insurance_Boolean_Logic_CSharp/Car_Insurance_Boolean_Logic_CSharp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_Insurance_Boolean_Logic_CSharp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age?");
+            int applicantAge = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI?");
+            string dui = Console.ReadLine();
+            Console.WriteLine("How many speeding tickets do you have?");
+            int speedTix = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Qualified:");
+            bool qualified = ((applicantAge >= 15) && (dui == "NO") || (dui == "no") && speedTix <= 3);
+            Console.WriteLine(qualified);
+            Console.ReadLine();
+        }
+    }
+}
