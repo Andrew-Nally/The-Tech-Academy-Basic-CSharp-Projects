57025451b4db157f0dca848ef57455dd72e7627b
Andrew-Nally <Andrew_Nally@outlook.com>
2020-10-23 20:36:28 -0600

    while/do_while loops assignment

---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main()
+        {
+            int count = 0;
+            while (count <= 5)
+            {
+                Console.WriteLine("The total = " + count);
+                count++;
+
+                Console.ReadLine();
+
+            }
+            int count2 = 0;
+            do
+            {
+                Console.WriteLine("Loopy");
+                count2++;
+            }
+            while (count2 <= 4);
+            Console.ReadLine();
+        }
+    }
+}
