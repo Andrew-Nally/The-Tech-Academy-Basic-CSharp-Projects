2838e485827811d38c797d7cc3b881e72025c554
Andrew-Nally <Andrew_Nally@outlook.com>
2020-10-24 14:10:28 -0600

    Strings

---
+++ StringFunctions/StringFunctions/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+      {
+            //        string name = "jesse";
+            //        string quote = "The man siad, \"hello\",Jesse. \n Hello on a new line. \n \t hello on a tab";
+            //        string fileName =@"C:\Users\Andre";
+
+            //        bool trueOrFalse = name.Contains("s");
+            //        trueOrFalse = name.EndsWith("s");
+
+            //        int length = name.Length;
+
+            //        name = name.ToUpper();
+
+            //        Console.WriteLine(fileName);
+            //        Console.ReadLine();
+
+            //string name = "Andrew";
+            //name = "Joe";
+
+            //Console.WriteLine(name);
+            //Console.ReadLine();
+            StringBuilder ab = new StringBuilder();
+            ab.Append("My name is Jesse");
+
+
+            Console.WriteLine(ab);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            StringBuilder sb = new StringBuilder("Hello, My name is Andrew.");
+            sb.AppendLine(" I like C#.");
+            sb.AppendLine("I also like The Tech Academy.");
+            sb.AppendLine("My favorite color is Red.");
+            sb.AppendLine("And I like to travel.");
+            
+            Console.WriteLine(sb);
+            Console.Read();
+
+        }
+    }
+}
