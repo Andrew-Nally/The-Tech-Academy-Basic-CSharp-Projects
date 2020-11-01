54e928ca4fad737fb147520ec467d874fc5c861c
Andrew-Nally <Andrew_Nally@outlook.com>
2020-11-01 13:59:17 -0700

    CLass_Object

---
+++ Class_Obj_Drill/Math.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Versioning;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Obj_Drill
+{
+    class Math
+    { 
+        public void Multiply(int num1)
+        {
+            int result = num1 * 5;
+            Console.WriteLine("Your number multiplied by 5 equals: " + result);
+        }
+        public void Addition(int num1)
+        {
+            int result = num1 + 5;
+            Console.WriteLine("Your number plus 5 equals: " + result);
+        }
+        public void Divide(int num1)
+        {
+            int result = num1 / 5;
+            Console.WriteLine("Your number divided by 5 equals: " + result);
+        }
+    }
+}
---
+++ Class_Obj_Drill/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Class_Obj_Drill
+
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Math math = new Math();
+            Console.WriteLine("Please enter a whole number:");
+            math.Multiply(Convert.ToInt32(Console.ReadLine()));
+            Console.ReadLine();
+
+            math = new Math();
+            Console.WriteLine("Please enter a whole number:");
+            math.Addition(Convert.ToInt32(Console.ReadLine()));
+            Console.ReadLine();
+
+            math = new Math();
+            Console.WriteLine("Please enter a whole number:");
+            math.Divide(Convert.ToInt32(Console.ReadLine()));
+            Console.ReadLine();
+        }
+    }
+}
