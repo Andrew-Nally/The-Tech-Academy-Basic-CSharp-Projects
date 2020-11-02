7e9a3a4c1d5a40cdb8a779a8aa2e89f1f11c65f8
Andrew-Nally <Andrew_Nally@outlook.com>
2020-11-01 19:08:40 -0700

    Void and Output Params

---
+++ Void_Output_Params/Math.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void_Output_Params
+{
+    class Math
+         {
+            public void mathApp(int num1) 
+        {
+            int results1 = num1 / 2;
+            Console.WriteLine(num1 + " / 2 = " + results1);
+        }
+
+        public static void mathApp(int num2, int num3, out int times, out int div)
+        {
+            times = num2 * num3;
+            div = (num2 / num3);
+        }
+
+    }
+}
+
---
+++ Void_Output_Params/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void_Output_Params
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+            Console.Write("enter a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            math.mathApp(num);
+
+            Console.Write("Enter another number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter one more number: ");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            Math.mathApp(num2, num3, out int times, out int div);
+
+            Console.WriteLine(num2 + " x " + num3 + " = " + times);
+            Console.WriteLine(num2 + " / " + num3 + " = " + div);
+            Console.ReadLine();
+
+        }
+    }
+}
