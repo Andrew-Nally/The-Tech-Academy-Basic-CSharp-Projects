9c672f80ef043b84d3ad19ba329264f96dc4a9f6
Andrew-Nally <Andrew_Nally@outlook.com>
2020-11-01 23:02:25 -0700

    Abstract class drill

---
+++ Abstract_Class_Drill/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class_Drill
+{
+    abstract class Person
+    { 
+        public string firstName;
+        public string lastName;
+        public virtual void SayName()
+        {
+            Console.WriteLine("Name: {0} {1}", firstName, lastName);
+        }
+    }
+}
---
+++ Abstract_Class_Drill/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class_Drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee per = new Employee();
+            per.firstName = "Sample";
+            per.lastName = "Student";
+            per.SayName();
+            Console.ReadLine();
+        }
+    }
+}
