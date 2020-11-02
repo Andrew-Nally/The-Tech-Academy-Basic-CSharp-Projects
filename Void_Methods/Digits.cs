039e6c4755b3447ff8905eae1a353c97b2e87c24
Andrew-Nally <Andrew_Nally@outlook.com>
2020-11-01 18:30:09 -0700

    Void_Methods/

---
+++ Void_Methods/Digits.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void_Methods
+{
+    class Digits
+    { 
+        public void DigitMethods(int num1, int num2)
+        {
+            int result = num1 * 75;
+            Console.WriteLine(num2);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Void_Methods/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Digits number = new Digits();
+                number.DigitMethods(3, 92);
+                int a = 9;
+                int b = 72;
+                number.DigitMethods(a, b);
+            }
+        }
+    }
+}
+
