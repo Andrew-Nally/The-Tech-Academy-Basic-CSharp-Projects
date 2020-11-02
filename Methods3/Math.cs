0009a608704cf0969857e17016f7a79cd2a65b54
Andrew-Nally <Andrew_Nally@outlook.com>
2020-11-01 18:23:29 -0700

    Option Parameters

---
+++ Methods3/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods3
+{
+    class Math
+    {
+        public int add(int num1, int num2)
+        {
+            int results = num1 + num2;
+            return results;
+        }
+    }
+}
---
+++ Methods3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+
+            Console.Write("Enter a number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please type in a second integer(optional): ");
+            int num2;
+            int results;
+            if (int.TryParse(Console.ReadLine(), out num2))
+            {
+                results = math.add(num1, num2);
+            }
+            else
+            {
+                results = num1;
+            }
+            Console.WriteLine("The answer is " + results);
+            Console.ReadLine();
+        }
+    }
+}
+ 
