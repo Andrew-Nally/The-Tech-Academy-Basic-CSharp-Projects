6270b664cc791a55461dda5bfd56b52a079f7614
Andrew-Nally <Andrew_Nally@outlook.com>
2020-11-02 15:37:39 -0700

    Enums

---
+++ Enums/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main()
+        {
+            DaysOfWeek day = getInput();
+            Console.WriteLine("Day of week: " + day);
+            Console.ReadLine();
+        }
+        public static DaysOfWeek getInput()
+        {
+            DaysOfWeek input;
+
+            Start:
+            try
+            {
+
+                Console.WriteLine("Please select a day of the week: ");
+                string rawInput = Console.ReadLine();
+                if (rawInput.All(char.IsDigit))
+                    throw new Exception();
+                input = (DaysOfWeek)Enum.Parse(typeof(DaysOfWeek), rawInput);
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+                goto Start;
+            }
+
+            return input;
+        }
+    }
+    enum DaysOfWeek
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+}
+   
