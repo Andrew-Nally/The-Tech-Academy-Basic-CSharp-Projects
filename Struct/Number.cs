04c9a9ecf4216446dcb41dc141b69e819518c3a1
Andrew-Nally <Andrew_Nally@outlook.com>
2020-11-02 16:07:31 -0700

    Struct Drill

---
+++ Struct/Number.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Struct
+{
+    struct Number
+    {
+        public double Amount
+        {
+            get; set;
+        }
+    }
+}
---
+++ Struct/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Struct
+{
+    class Program
+    {  
+           public static void Main(string[] args)
+        {
+            Number num = new Number();
+            num.Amount = 5.2591;
+            Console.WriteLine("{0}", num.Amount);
+            Console.ReadLine();
+        }
+    }
+}
