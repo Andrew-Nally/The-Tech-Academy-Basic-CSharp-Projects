a9a1d08c91b2d266797dd0f5e87b56f92d0e5e18
Andrew-Nally <Andrew_Nally@outlook.com>
2020-11-02 00:32:22 -0700

    IQuittable

---
+++ Interface_IQuttable/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_IQuttable
+{
+    class Employee : Person, IQuittable
+    {
+        int Id = 0;
+        public Employee(string FirstName, string LastName) : base(FirstName, LastName)
+        {
+
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("Press Enter to Exit");
+            Console.Read();
+            Environment.Exit(0);
+        }
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + "[" + base.FirstName + " " + base.LastName + "]");
+        }
+    }
+}
+   
