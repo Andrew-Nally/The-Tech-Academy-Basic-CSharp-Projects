34338950f39695b11ad64f88e21b28da9a7279a2
Andrew-Nally <Andrew_Nally@outlook.com>
2020-11-01 18:03:53 -0700

    Method Overload

---
+++ Methods2/Math.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods2
+{
+    class Math
+    {
+        public void Divide(int num1)
+            {
+                int result = num1 / 2;
+                Console.WriteLine("Your number divided by 2 equals: " + result);
+            }
+            public void Divide(decimal num2)
+            {
+                int result = Convert.ToInt32(num2 * 3);
+                Console.WriteLine("Your number multiplied by 3 equals: " + result);
+            }
+            public void Divide(string name)
+            {
+                int x = name.Length;
+                int result = Convert.ToInt32(x + 5);
+                Console.WriteLine("The number of letters in your name + 5 equals: " + result);
+            }
+
+        }
+    }
---
+++ Methods2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {        
+                Math math = new Math();
+                Console.WriteLine("Enter a whole number: ");
+                math.Divide(Convert.ToInt32(Console.ReadLine()));
+                Console.ReadLine();
+
+                math = new Math();
+                Console.WriteLine("Enter a decimal number: ");
+                math.Divide(Convert.ToDecimal(Console.ReadLine()));
+                Console.ReadLine();
+
+                math = new Math();
+                Console.WriteLine("Enter Your Name: ");
+                math.Divide(Convert.ToString(Console.ReadLine()));
+                Console.ReadLine();
+
+            }
+        }
+    }
