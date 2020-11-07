dff334e65592431682847c66fe55ca84a1bd1113
Andrew-Nally <Andrew_Nally@outlook.com>
2020-11-07 12:37:02 -0700

    DateTime Drill

---
+++ DateTime/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Date_Time_Drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime rightNow = DateTime.Now;
+            Console.WriteLine("It is currently: " + rightNow);
+
+            Console.WriteLine("\nEnter a whole number:");
+            var userNum = Convert.ToInt32(Console.ReadLine());
+
+            rightNow = rightNow.AddHours(userNum);
+            Console.WriteLine("\nIn {0} hours it will be: {1}.", userNum, rightNow);
+            Console.ReadLine();
+        }
+    }
+}
