3022e83df305e39dc99502b724a7f114d06824ef
Andrew-Nally <Andrew_Nally@outlook.com>
2020-11-07 12:47:26 -0700

    Constructor Drill

---
+++ Constuctor_Drill/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_var
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string ClothingStore = "Fashion Barn";
+            var Location = "Chicago, IL";
+            Store fb259 = new Store(259, "Gold Coast");
+            Store fb092 = new Store(092, "South Loop");
+            Console.WriteLine("In recent news, " + ClothingStore + " will be opening 2 new locations in : " + Location + "\n" + " Store 259 in the " + fb259.location + " \n Store 092 in the " + fb092.location + "\n Thank you for all the hard work put into opening both of these beautiful locations!\n Congratulations and have a great season! \n" + "\n FASHION BARN CARES 4 YOU!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Constuctor_Drill/store.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_var
+{
+    class Store
+    {
+        public int id;
+        public string location;
+        public Store() : this(0, "") { }
+        public Store(int id, string location)
+        {
+            this.id = id;
+            this.location = location;
+        }
+        public Store(int id) : this(id, "") { }
+        public Store(string location) : this(0, location) { }
+    }
+}
