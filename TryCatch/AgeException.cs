790c1b66ec06fe0da177f06079c9ebf1a1b7170e
Andrew-Nally <Andrew_Nally@outlook.com>
2020-11-07 12:53:21 -0700

    TRY/CATCH

---
+++ TryCatch/AgeException.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Age_TryCatch
+{
+    public class AgeException : Exception
+    {
+        public AgeException()
+           : base() { }
+        public AgeException(String message)
+            : base(message) { }
+    }
+}
